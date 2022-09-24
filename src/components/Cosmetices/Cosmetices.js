d016bc4d3f6c21e173c5551bf39dfa924784e366
bd-dev-ismail <ismailratul7@gmail.com>
2022-09-24 17:45:32 +0600

    added

---
+++ src/components/Cosmetices/Cosmetices.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetices = () => {
+    return (
+        <div>
+            <h1>Welcome to our cosmetic store</h1>
+        </div>
+    );
+};
+
+export default Cosmetices;
